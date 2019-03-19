8a8f9ba88a09479d53ad15a2db1cdd70357f88b7
Denis <denis.erfurt@gmail.com>
2019-03-19 22:05:20 +0100

    init2

--- src/easyNest.sol
+++ src/easyNest.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.5.3;
 contract Callee {
   uint temperature = 0;
   function tempDelta(uint x) public {
     temperature = add(temperature, x);
   }
-  
+
   function add(uint x, uint y) internal pure returns (uint z) {
     z = x + y;
     require(z >= x);
